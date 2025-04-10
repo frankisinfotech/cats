5e214e2e81de36be3958488fdf50a61d5a13bd72
frankisinfotech <67714070+frankisinfotech@users.noreply.github.com>
2025-04-10 19:53:49 +0100

    Update worker.tf

--- terraform/eks/worker.tf
+++ terraform/eks/worker.tf
@@ -6,7 +6,7 @@ resource "aws_eks_node_group" "eksnode" {
   cluster_name    = var.eks_cluster
   node_group_name = "eksnodegroup"
   node_role_arn   = aws_iam_role.eksnoderole.arn
-  subnet_ids      = ["subnet-00f882502a1585aed", "subnet-0f7dd8c02c7b3701f"]
+  subnet_ids      = ["subnet-0c83cfeb00c79be5d", "subnet-079abcbbc8eef499f", "subnet-0bb5ed3bf4d34b3a0"]
 
   scaling_config {
     desired_size = 2
